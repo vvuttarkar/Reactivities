18d9ecb4b124ea383e1ad9540a56b43d5fce8e0b
vvuttarkar <50836895+vvuttarkar@users.noreply.github.com>
2021-11-30 19:52:53 +0530

    Backup

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,34 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../../models/activity';
+import NavBar from './Navbar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    });
+
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))
+          }
+        </List>
+      </Container>
+    </>
+  );
+}
+
+export default App;
