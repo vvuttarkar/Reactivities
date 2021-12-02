0488ed3cbbaefc0e0432052a68c9dd5c5272bba5
Vinayak Uttarkar <M1052421@mindtree.com>
2021-12-02 18:34:17 +0530

    Uninstall

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../../models/activity';
 import NavBar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -18,14 +19,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <List>
-          {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))
-          }
-        </List>
+        <ActivityDashboard activities={activities}></ActivityDashboard>
       </Container>
     </>
   );
